bb12197f6d009f50b13fc2b53ed4dcad3024b15d
YufengHu <iosyufeng@sina.com>
2016-07-21 16:59:44 +0800

    a
    a

---
+++ XMPPDeveloping/XMPPDeveloping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPDeveloping
+//
+//  Created by 万商维盟 on 16/7/21.
+//  Copyright © 2016年 万商维盟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPDeveloping/XMPPDeveloping/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPDeveloping
+//
+//  Created by 万商维盟 on 16/7/21.
+//  Copyright © 2016年 万商维盟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
